fcf021ebf8e5a3f848592c9eac1b97a1828b74ab
Ipseeta Priyadarshini <ipseeta.pkar@gmail.com>
2021-05-27 14:17:52 +0530

    array of

---
+++ array/arrayof.js
@@ -0,0 +1,13 @@
+console.log(Array.of(7));
+console.log(Array(7));
+console.log(Array.of(undefined));
+console.log(Array.of(1,2,3));
+
+/**
+ * O/Ps
+[ 7 ]
+[ <7 empty items> ]
+[ undefined ]
+[ 1, 2, 3 ]
+The Array.of() method creates a new Array instance from a variable number of arguments, regardless of number or type of the arguments.
+ */
