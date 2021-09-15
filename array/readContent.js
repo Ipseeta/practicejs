4fbe3ba97a06734361e484b061e81eb373223154
Ipseeta Priyadarshini <ipseeta.pkar@gmail.com>
2021-09-15 16:31:09 +0530

    practice

---
+++ array/readContent.js
@@ -0,0 +1,31 @@
+// Read sequentially the content for given array of files
+const fs = require('fs');
+const files = [
+    '0.txt',
+    '1.txt',
+    '2.txt',
+    '3.txt',
+    '4.txt'];
+
+const readFromFile = (file) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(`files/${file}`, (err, data) => {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            else {
+                resolve(data.toString());
+            }
+        });
+    });
+}
+
+(async () => {
+    try {
+        const response = await Promise.all(files.map(file => readFromFile(file)));
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+})();    
